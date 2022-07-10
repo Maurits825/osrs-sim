269b0d2111e22bf8ddcd1002af8d75d71ca782ff
Maurits <63048736+Maurits825@users.noreply.github.com>
2022-07-10 18:21:00 +0200

    added camera rotating and zooming

--- OSRS Sim/Assets/Scripts/CameraController.cs
+++ OSRS Sim/Assets/Scripts/CameraController.cs
@@ -5,12 +5,45 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public Transform player;
-    private float maxCameraSpeed = 2.5f;
+    private Vector3 playerOffset = Vector3.up * 1f;
 
-    // Update is called once per frame
-    void Update()
+    private float rotateSpeed = 0.2f;
+    private Vector2 previousMousePos;
+    private Vector2 currentMousePos;
+
+    private float zoom = -5f;
+    private float zoomSpeed = 0.9f;
+    private float minZoom = -100f;
+    private float maxZoom = -2f;
+
+    private void Start()
     {
-        Vector3 newPos = player.transform.position + new Vector3(0, 5, -5);
-        transform.position = Vector3.Slerp(transform.position, newPos, maxCameraSpeed * Time.deltaTime);
+        transform.LookAt(player.transform.position + playerOffset);
+    }
+    private void Update()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        if (Input.GetMouseButtonDown(2))
+        {
+            previousMousePos = new Vector2(mousePos.x, mousePos.y);
+        }
+        
+        if (Input.GetMouseButton(2))
+        { 
+            currentMousePos = new Vector2(mousePos.x, mousePos.y);
+
+            Vector2 mousePosDiff = currentMousePos - previousMousePos;
+            transform.RotateAround(player.transform.position + playerOffset, Vector3.up, mousePosDiff.x * rotateSpeed);
+            transform.RotateAround(player.transform.position + playerOffset, -transform.right, mousePosDiff.y * rotateSpeed);
+            previousMousePos = currentMousePos;
+        }
+
+        if (Input.mouseScrollDelta.y != 0)
+        {
+            zoom += Input.mouseScrollDelta.y * zoomSpeed;
+            zoom = Mathf.Clamp(zoom, minZoom, maxZoom);
+        }
+
+        transform.position = player.transform.position + playerOffset + (zoom * transform.forward);
     }
 }
